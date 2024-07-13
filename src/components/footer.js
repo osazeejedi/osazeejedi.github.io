7a4d9b3d7598c7676dfa84263563fc6654266d9c
Osazee Jedidiah .O. <63594399+osazeejedi@users.noreply.github.com>
2024-07-13 01:53:31 +0000

    foot

--- src/components/footer.js
+++ src/components/footer.js
@@ -106,7 +106,7 @@ const Footer = () => {
           target="_blank"
           rel="nofollow noopener noreferrer">
           <div>
-            Designed &amp; Built by Britt<br></br>
+            Designed &amp; Built by Brit<br></br>
             Revised by Osazee Jedidiah O.
           </div>
 
